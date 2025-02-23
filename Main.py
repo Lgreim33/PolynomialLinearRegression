96c6b1e7d2e58ae9cb96c937f1594c7cc2b18c5a
Liam Greim <lgreim33@gmail.com>
2025-02-22 19:21:22 -0600

    backpropis being implemented

--- Main.py
+++ Main.py
@@ -1,10 +1,25 @@
 from regression import LineaerRegression
 import kagglehub
-
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+import numpy as np
 
 # get dataset
 path = kagglehub.dataset_download("abhishek14398/salary-dataset-simple-linear-regression")
 
-regressor = LineaerRegression(2)
+regressor = LineaerRegression()
 
-print(regressor.poly)
+polynomial = regressor.poly_coefficients
+polynomial.reverse()
+
+data = pd.read_csv(path + "\\" +  os.listdir(path)[0] )
+print(data)
+x_values = np.linspace(0,40,regressor.order+1)
+# show data
+plt.plot(data["YearsExperience"],data["Salary"], marker='o' , linestyle=' ')
+# show our line of best fit pre fit
+plt.plot(x_values,polynomial)
+
+plt.plot(1.2,regressor.forward(1.2),marker='o' , linestyle=' ')
+plt.show()
